e5c54eb4b144e35d40995c5e9b0ddab1a4e9759f
A-Bun <alexisp30ples@gmail.com>
2021-02-10 10:02:11 -0500

    Fixed Fonts and Added Resources Folder

---
+++ Assets/Scripts/exitScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitScript : MonoBehaviour
+{
+    /* Update
+     *   Checks if the backspace key was pressed and quits the game.
+     */
+    void Update()
+    {
+        if (Input.GetKeyDown("backspace"))
+        {
+            Quit();
+        }
+    }
+
+    /* Quit
+     *   Prints a quit message to the console in the unity player and completely quits
+     *   the game in the build.
+     */
+    public void Quit()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
