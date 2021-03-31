4a1d51158270707f824cee615948207b5ab00ee0
A-Bun <alexisp30ples@gmail.com>
2021-03-30 23:30:44 -0400

    Tutorial 3/4 Complete
    A bit more of the tutorial is complete.

--- Assets/Scripts/GameScripts/Tutorial.cs
+++ Assets/Scripts/GameScripts/Tutorial.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Tutorial : MonoBehaviour
 {
     public Player player;
     public GameObject personUI, blocker, songbookPanel, shopPanel, boxSB, boxShop, boxLap;
-    public Button songbookButton, shopButton, laptop;
+    public Button songbookButton, shopButton, laptopButton, buyButton;
     public DialogueManager manager;
-    public Sprite redBox;
     public List<Dialogue> dialogues = new List<Dialogue>();
-    private bool sbStart, sbEnd, shopStart, shopEnd, lapStart, lapEnd;
+    private bool sbStart, sbEnd, shopStart, shopEnd, lapStart, lapEnd, dial2, dial5;
     private Image image;
     private GameObject textBox;
     private Vector3 orgPos, pos;
@@ -29,7 +29,8 @@ public class Tutorial : MonoBehaviour
             blocker.SetActive(true);
             songbookButton.enabled = false;
             shopButton.enabled = false;
-            laptop.enabled = false;
+            laptopButton.enabled = false;
+            buyButton.enabled = false;
             songbookPanel.transform.GetChild(3).gameObject.SetActive(false);
             shopPanel.transform.GetChild(1).gameObject.SetActive(false);
             StartCoroutine(WaitForDialogue(dialogues[0]));
@@ -42,27 +43,53 @@ public class Tutorial : MonoBehaviour
         {
             boxSB.SetActive(true);
         }
-        //else if(manager.currSentence == dialogues[2].sentences[0])
+        else if(manager.currSentence == dialogues[2].sentences[0])
+        {
+            boxShop.SetActive(true);
+        }
+        else if(dialogues[5].isComplete)
+        {
+            songbookButton.enabled = true;
+            laptopButton.enabled = true;
+        }    
 
         if (dialogues[0].isComplete && !sbStart)
         {
             songbookButton.onClick.AddListener(HandleSongbook);
             songbookButton.enabled = true;
         }
-        //else if (sbEnd && !songbookPanel.activeInHierarchy && !shopStart)
-        //{
-        //    ResetTextBox();
-        //    manager.StartDialogue(dialogues[2]);
-        //}
+        else if (sbEnd && !songbookPanel.activeInHierarchy && !dial2)
+        {
+            dial2 = true;
+            ResetTextBox();
+            manager.StartDialogue(dialogues[2]);
+        }
+        else if(dialogues[2].isComplete && !shopStart)
+        {
+            shopButton.onClick.AddListener(HandleShop);
+            shopButton.enabled = true;
+        }
+        else if (shopEnd && !shopPanel.activeInHierarchy && !dial5)
+        {
+            dial5 = true;
+            ResetTextBox();
+            manager.StartDialogue(dialogues[5]);
+        }
 
         if (sbStart && !sbEnd)
         {
             HandleSongbook();
         }
+        else if(shopStart && !shopEnd)
+        {
+            HandleShop();
+        }
     }
 
     public void HandleSongbook()
     {
+        GameObject temp = songbookPanel.transform.GetChild(2).gameObject;
+
         if (!sbStart)
         {
             sbStart = true;
@@ -87,15 +114,52 @@ public class Tutorial : MonoBehaviour
             else if (manager.currSentence == dialogues[1].sentences[4])
             {
                 image.enabled = false;
-                songbookPanel.transform.GetChild(2).gameObject.SetActive(true);
+                temp.SetActive(true);
             }
 
             if (dialogues[1].isComplete)
             {
+                temp.SetActive(false);
+                songbookButton.enabled = false;
                 songbookPanel.transform.GetChild(3).gameObject.SetActive(true);
                 sbEnd = true;
             }
         }
+    }
+
+    public void HandleShop()
+    {
+        if (!shopStart)
+        {
+            shopStart = true;
+            pos = new Vector3(0f, 0f, 0f);
+            MoveTextBox(shopPanel.transform, pos);
+
+            manager.StartDialogue(dialogues[3]);
+            boxShop.SetActive(false);
+            buyButton.onClick.AddListener(SaveSongDialogue);
+        }
+
+        if (shopEnd == false)
+        {
+            if(dialogues[3].isComplete)
+            {
+                buyButton.enabled = true;
+            }
+
+            if (dialogues[4].isComplete)
+            {
+                buyButton.enabled = false;
+                shopPanel.transform.GetChild(1).gameObject.SetActive(true);
+                shopEnd = true;
+            }
+        }
+    }
+
+    private void SaveSongDialogue()
+    {
+        buyButton.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "SOLD";
+        manager.StartDialogue(dialogues[4]);
     }
 
     public void MoveTextBox(Transform newParent, Vector3 newPos)
